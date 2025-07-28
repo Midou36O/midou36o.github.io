249a6c88f456a1ca3abb1854bdbf9a38f7aca6a3
Midou <midou@midou.dev>
2025-07-29 00:48:08 +0100

    SKG is successful, it's no longer needed.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,10 @@
 	import Foote from '$lib/foote.svelte';
 	import '@unocss/reset/tailwind.css';
 	import Navi from '$lib/navi.svelte';
-	import Banner from '$lib/banner.svelte';
 </script>
 
 <body>
-	<Banner message="If you are an european, please save gaming by signing this initiative!" link="https://www.stopkillinggames.com/eci" />
 	<main class="dark:bg-black bg-gray-300 overflow-hidden">
 		<slot />
 	</main>
 </body>
-
